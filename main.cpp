4eadb4dd8b6f3f048178de2823a1864fe1dba786
amdreallyfast <amdreallyfast@hotmail.com>
2016-06-12 09:44:03 -0600

    Now rendering multiple shapes
    I re-wrote some things, adding in a primitive generator and geometry
    data based on what I put into my main sandbox program, although
    streamline, and moved the OpenGL error handling debug callback function
    off into its own area.

--- main.cpp
+++ main.cpp
@@ -38,230 +38,22 @@
 // for printf(...)
 #include <stdio.h>
 
+#include "OpenGlErrorHandling.h"
+#include "GenerateShader.h"
+#include "GeometryData.h"
+#include "PrimitiveGeneration.h"
 
-void APIENTRY DebugFunc(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
-    const GLchar* message, const GLvoid* userParam)
-{
-    std::string srcName;
-    switch (source)
-    {
-    case GL_DEBUG_SOURCE_API_ARB: srcName = "API"; break;
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB: srcName = "Window System"; break;
-    case GL_DEBUG_SOURCE_SHADER_COMPILER_ARB: srcName = "Shader Compiler"; break;
-    case GL_DEBUG_SOURCE_THIRD_PARTY_ARB: srcName = "Third Party"; break;
-    case GL_DEBUG_SOURCE_APPLICATION_ARB: srcName = "Application"; break;
-    case GL_DEBUG_SOURCE_OTHER_ARB: srcName = "Other"; break;
-    }
+GLuint gProgramId;
+GeometryData gTriangle;
+GeometryData gBox;
+GeometryData gCircle;
+GLint gUniformLocation;
 
-    std::string errorType;
-    switch (type)
-    {
-    case GL_DEBUG_TYPE_ERROR_ARB: errorType = "Error"; break;
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB: errorType = "Deprecated Functionality"; break;
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB: errorType = "Undefined Behavior"; break;
-    case GL_DEBUG_TYPE_PORTABILITY_ARB: errorType = "Portability"; break;
-    case GL_DEBUG_TYPE_PERFORMANCE_ARB: errorType = "Performance"; break;
-    case GL_DEBUG_TYPE_OTHER_ARB: errorType = "Other"; break;
-    }
 
-    std::string typeSeverity;
-    switch (severity)
-    {
-    case GL_DEBUG_SEVERITY_HIGH_ARB: typeSeverity = "High"; break;
-    case GL_DEBUG_SEVERITY_MEDIUM_ARB: typeSeverity = "Medium"; break;
-    case GL_DEBUG_SEVERITY_LOW_ARB: typeSeverity = "Low"; break;
-    }
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
-    printf("%s from %s,\t%s priority\nMessage: %s\n",
-        errorType.c_str(), srcName.c_str(), typeSeverity.c_str(), message);
-}
-
-GLuint CreateGeometry()
-{
-    // this doesn't strictly need to be static because the gl*Data(...) functions (called later 
-    // in this same function) will send it off to the GPU, and then it won't be needed in system 
-    // memory
-    GLfloat localVerts[] =
-    {
-        -0.5f, -0.5f, -1.0f,          // (pos) left bottom corner
-        +1.0f, +0.0f, +0.0f,          // (color) red
-
-        +0.5f, -0.5f, -1.0f,          // (pos) right bottom corner
-        +0.0f, +1.0f, +0.0f,          // (color) green
-
-        +0.0f, +0.5f, -1.0f,          // (pos) center top
-        +0.0f, +0.0f, +1.0f,          // (color) blue
-    };
-
-    // set the argument vertex array as the active one
-    GLuint vertexArrayObjectId = 0;
-    glGenVertexArrays(1, &vertexArrayObjectId);
-    glBindVertexArray(vertexArrayObjectId);
-
-    // create vertex data
-    // Note: If you need to later call glDeleteBuffers(...) to clean up the data, keep 
-    // the vertex buffer ID around.  This barebones code will rely on program end for
-    // cleanup, and the bound VAO will handle future binding/unbinding, so the ID will 
-    // be created just long enough to use it.
-    GLuint vertBufId = 0;
-    glGenBuffers(1, &vertBufId);
-    glBindBuffer(GL_ARRAY_BUFFER, vertBufId);  // ??check for bad number first??
-
-    // send data to GPU
-    // Note: If data already exists and you know exactly which byte in the array to stick the 
-    // new data, use glBufferSubData(...)
-    glBufferData(GL_ARRAY_BUFFER, sizeof(localVerts), localVerts, GL_STATIC_DRAW);
-
-    // set vertex array data to describe the byte pattern
-    // Note: The byte pattern in that single float array is arranged such that it can be split 
-    // into three distinct patterns.  Vertex Attribute Arrays let the patterns be specified.
-    // Starting at float 0, the pattern repeats every 9 floats (36 bytes) until end of the 
-    // buffer.  OpenGL already knows the data size from glBufferData(...).
-    unsigned int vertexArrayIndex = 0;
-    unsigned int bufferStartByteOffset = 0;
-    const unsigned int NUM_THINGS = 3;          // 3 floats per vertex array index
-    const unsigned int BYTES_PER_THING = 12;    // 3 floats at 4 bytes per
-    const unsigned int BYTES_PER_VERT = 24;     // 3 pos + 3 color
-
-    GLubyte *pB = (GLubyte *)localVerts;
-    GLfloat *pF = (GLfloat *)pB;;
-    pB += BYTES_PER_THING;
-    pF = (GLfloat *)pB;
-    pB += BYTES_PER_THING;
-    pF = (GLfloat *)pB;
-
-    // position 
-    glEnableVertexAttribArray(vertexArrayIndex);
-    glVertexAttribPointer(vertexArrayIndex, NUM_THINGS, GL_FLOAT, GL_FALSE,
-        BYTES_PER_VERT, (void *)bufferStartByteOffset);
-    bufferStartByteOffset += BYTES_PER_THING;   // byte offset now 12
-
-    // color
-    // Note: Don't forget (as I did) to increment to the next component or the color in the 
-    // vertex shader will be a zero vector (that is, won't get set).
-    vertexArrayIndex++; 
-    glEnableVertexAttribArray(vertexArrayIndex);
-    glVertexAttribPointer(vertexArrayIndex, NUM_THINGS, GL_FLOAT, GL_FALSE,
-        BYTES_PER_VERT, (void *)bufferStartByteOffset);
-
-    // index data
-    // Note: In order to draw, OpenGL needs point data.  Triangles always need three points 
-    // specified, so rather than sending in repeat vertices, index data allows the user to 
-    // repeat index data instead (always in sets of 3), which is computationally less demanding 
-    // than sending the entire vertex to the GPU multiple times.  The vertices were previously 
-    // described by the vertex attribute arrays and associated pointers, saying that each vertex 
-    // was composed of three things with their own byte patterns.
-    GLushort localIndices[]
-    {
-        0, 1, 2,
-    };
-
-    // like "vertex buffer ID", keep this around if you need to alter the data or clean it up 
-    // properly before program end
-    GLuint elemArrBufId = 0;
-    glGenBuffers(1, &elemArrBufId);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elemArrBufId);  // ??check for bad number first??
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(localIndices), localIndices, GL_STATIC_DRAW);
-
-    // clean up bindings
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // all good, so return the VAO ID
-    return vertexArrayObjectId;
-}
-
-GLuint CreateProgram()
-{
-    // hard-coded ignoring possible errors like a boss
-
-    // load up the vertex shader and compile it
-    // Note: After retrieving the file's contents, dump the stringstream's contents into a 
-    // single std::string.  Do this because, in order to provide the data for shader 
-    // compilation, pointers are needed.  The std::string that the stringstream::str() function 
-    // returns is a copy of the data, not a reference or pointer to it, so it will go bad as 
-    // soon as the std::string object disappears.  To deal with it, copy the data into a 
-    // temporary string.
-    std::ifstream shaderFile("shader.vert");
-    std::stringstream shaderData;
-    shaderData << shaderFile.rdbuf();
-    shaderFile.close();
-    std::string tempFileContents = shaderData.str();
-    GLuint vertShaderId = glCreateShader(GL_VERTEX_SHADER);
-    const GLchar *vertBytes[] = { tempFileContents.c_str() };
-    const GLint vertStrLengths[] = { (int)tempFileContents.length() };
-    glShaderSource(vertShaderId, 1, vertBytes, vertStrLengths);
-    glCompileShader(vertShaderId);
-    // alternately (if you are willing to include and link in glutil, boost, and glm), call 
-    // glutil::CompileShader(GL_VERTEX_SHADER, shaderData.str());
-
-    GLint isCompiled = 0;
-    glGetShaderiv(vertShaderId, GL_COMPILE_STATUS, &isCompiled);
-    if (isCompiled == GL_FALSE)
-    {
-        GLchar errLog[128];
-        GLsizei *logLen = 0;
-        glGetShaderInfoLog(vertShaderId, 128, logLen, errLog);
-        printf("vertex shader failed: '%s'\n", errLog);
-        glDeleteShader(vertShaderId);
-        return 0;
-    }
-
-    // load up the fragment shader and compiler it
-    shaderFile.open("shader.frag");
-    shaderData.str(std::string());      // because stringstream::clear() only clears error flags
-    shaderData.clear();                 // clear any error flags that may have popped up
-    shaderData << shaderFile.rdbuf();
-    shaderFile.close();
-    tempFileContents = shaderData.str();
-    GLuint fragShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-    const GLchar *fragBytes[] = { tempFileContents.c_str() };
-    const GLint fragStrLengths[] = { (int)tempFileContents.length() };
-    glShaderSource(fragShaderId, 1, fragBytes, fragStrLengths);
-    glCompileShader(fragShaderId);
-
-    glGetShaderiv(fragShaderId, GL_COMPILE_STATUS, &isCompiled);
-    if (isCompiled == GL_FALSE)
-    {
-        GLchar errLog[128];
-        GLsizei *logLen = 0;
-        glGetShaderInfoLog(fragShaderId, 128, logLen, errLog);
-        printf("fragment shader failed: '%s'\n", errLog);
-        glDeleteShader(vertShaderId);
-        glDeleteShader(fragShaderId);
-        return 0;
-    }
-
-    GLuint programId = glCreateProgram();
-    glAttachShader(programId, vertShaderId);
-    glAttachShader(programId, fragShaderId);
-    glLinkProgram(programId);
-
-    // the program contains binary, linked versions of the shaders, so clean up the compile 
-    // objects
-    // Note: Shader objects need to be un-linked before they can be deleted.  This is ok because
-    // the program safely contains the shaders in binary form.
-    glDetachShader(programId, vertShaderId);
-    glDetachShader(programId, fragShaderId);
-    glDeleteShader(vertShaderId);
-    glDeleteShader(fragShaderId);
-
-    // check if the program was built ok
-    GLint isLinked = 0;
-    glGetProgramiv(programId, GL_LINK_STATUS, &isLinked);
-    if (isLinked == GL_FALSE)
-    {
-        printf("program didn't compile\n");
-        glDeleteProgram(programId);
-        return 0;
-    }
-
-    // done here
-    return programId;
-}
-
-void init()
+void Init()
 {
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -272,60 +64,55 @@ void init()
     glDepthFunc(GL_LEQUAL);
     glDepthRange(0.0f, 1.0f);
 
-    GLuint programId = CreateProgram();
-    glUseProgram(programId);
+    gProgramId = GenerateShaderProgram();
+    //glm::mat4 m = glm::translate(glm::mat4(), glm::vec3(+0.25f, +0.25f, 0.0f));
+    //glm::mat4 m = glm::translate(glm::mat4(), glm::vec3(+0.0f, +0.0f, 0.0f));
+    gUniformLocation = glGetUniformLocation(gProgramId, "translateMatrixWindowSpace");
+    //glUniformMatrix4fv(gUniformLocation, 1, GL_FALSE, glm::value_ptr(m));
 
-    GLuint vaoId = CreateGeometry();
-    glBindVertexArray(vaoId);
+    GenerateTriangle(&gTriangle);
+
+    GenerateBox(&gBox);
+    GenerateCircle(&gCircle);
+    InitializeGeometry(gProgramId, &gTriangle);
+    InitializeGeometry(gProgramId, &gBox);
+    InitializeGeometry(gProgramId, &gCircle);
 }
 
 //Called to update the display.
 //You should call glutSwapBuffers after all of your rendering to display what you rendered.
 //If you need continuous updates of the screen, call glutPostRedisplay() at the end of the function.
-void display()
+void Display()
 {
-    //g_scene.update();
-    //g_renderer.render_scene();
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClearDepth(1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
-    GLenum err = glGetError();
-    if (GL_NO_ERROR != err)
-    {
-        switch (err)
-        {
-        case GL_INVALID_ENUM:
-            printf("GL error: GL_INVALID_ENUM\n");
-            break;
-        case GL_INVALID_VALUE:
-            printf("GL error: GL_INVALID_VALUE\n");
-            break;
-        case GL_INVALID_OPERATION:
-            printf("GL error: GL_INVALID_OPERATION\n");
-            break;
-        case GL_STACK_OVERFLOW:
-            printf("GL error: GL_STACK_OVERFLOW\n");
-            break;
-        case GL_STACK_UNDERFLOW:
-            printf("GL error: GL_STACK_UNDERFLOW\n");
-            break;
-        case GL_OUT_OF_MEMORY:
-            printf("GL error: GL_OUT_OF_MEMORY\n");
-            break;
-        case GL_INVALID_FRAMEBUFFER_OPERATION:
-            printf("GL error: GL_INVALID_FRAMEBUFFER_OPERATION\n");
-            break;
-        //case GL_CONTEXT_LOST: // OpenGL 4.5 or higher
-        //    break;
-        case GL_TABLE_TOO_LARGE:
-            printf("GL error: GL_TABLE_TOO_LARGE\n");
-            break;
-        default:
-            printf("GL error: UNKNOWN\n");
-            break;
-        }
-    }
+
+    glm::mat4 translateMatrix;
+
+    glUseProgram(gProgramId);
+
+    // put the triangle up and to the right
+    // Note: Remember that this program is "barebones", so translation must be in window space 
+    // ([-1,+1] on X and Y).
+    translateMatrix = glm::translate(glm::mat4(), glm::vec3(+0.3f, +0.3f, 0.0f));
+    glUniformMatrix4fv(gUniformLocation, 1, GL_FALSE, glm::value_ptr(translateMatrix));
+    glBindVertexArray(gTriangle._vaoId);
+    glDrawElements(gTriangle._drawStyle, gTriangle._indices.size(), GL_UNSIGNED_SHORT, 0);
+
+    // put the box up and to the left
+    translateMatrix = glm::translate(glm::mat4(), glm::vec3(-0.3f, +0.3f, 0.0f));
+    glUniformMatrix4fv(gUniformLocation, 1, GL_FALSE, glm::value_ptr(translateMatrix));
+    glBindVertexArray(gBox._vaoId);
+    glDrawElements(gBox._drawStyle, gBox._indices.size(), GL_UNSIGNED_SHORT, 0);
+
+    // put the circle down and center
+    translateMatrix = glm::translate(glm::mat4(), glm::vec3(0.0f, -0.3f, 0.0f));
+    glUniformMatrix4fv(gUniformLocation, 1, GL_FALSE, glm::value_ptr(translateMatrix));
+    glBindVertexArray(gCircle._vaoId);
+    glDrawElements(gCircle._drawStyle, gCircle._indices.size(), GL_UNSIGNED_SHORT, 0);
+
+    glUseProgram(0);
 
     glutSwapBuffers();
     glutPostRedisplay();
@@ -333,7 +120,7 @@ void display()
 
 //Called whenever the window is resized. The new window size is given, in pixels.
 //This is an opportunity to call glViewport or glScissor to keep up with the change in size.
-void reshape(int w, int h)
+void Reshape(int w, int h)
 {
     glViewport(0, 0, w, h);
 }
@@ -342,8 +129,10 @@ void reshape(int w, int h)
 //The key is given by the ''key'' parameter, which is in ASCII.
 //It's often a good idea to have the escape key (ASCII value 27) call glutLeaveMainLoop() to 
 //exit the program.
-void keyboard(unsigned char key, int x, int y)
+void Keyboard(unsigned char key, int x, int y)
 {
+    // this statement is mostly to get ride of an "unreferenced parameter" warning
+    printf("keyboard: x = %d, y = %d\n", x, y);
     switch (key)
     {
     case 27:
@@ -357,13 +146,17 @@ void keyboard(unsigned char key, int x, int y)
     }
 }
 
+// I don't know what this does, but I've kept it around since early times, and this was the
+// comment given with it:
 //Called before FreeGLUT is initialized. It should return the FreeGLUT
 //display mode flags that you want to use. The initial value are the standard ones
 //used by the framework. You can modify it or just return you own set.
 //This function can also set the width/height of the window. The initial
 //value of these variables is the default, but you can change it.
-unsigned int defaults(unsigned int displayMode, int &width, int &height) 
-{ 
+unsigned int Defaults(unsigned int displayMode, int &width, int &height) 
+{
+    // this statement is mostly to get ride of an "unreferenced parameter" warning
+    printf("Defaults: width = %d, height = %d\n", width, height);
     return displayMode; 
 }
 
@@ -374,14 +167,18 @@ int main(int argc, char *argv[])
     int width = 500;
     int height = 500;
     unsigned int displayMode = GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH | GLUT_STENCIL;
-    displayMode = defaults(displayMode, width, height);
+    displayMode = Defaults(displayMode, width, height);
 
     glutInitDisplayMode(displayMode);
     glutInitContextVersion(4, 4);
     glutInitContextProfile(GLUT_CORE_PROFILE);
+
+    // enable this for automatic message reporting (see OpenGlErrorHandling.cpp)
+//#define DEBUG
 #ifdef DEBUG
     glutInitContextFlags(GLUT_DEBUG);
 #endif
+
     glutInitWindowSize(width, height);
     glutInitWindowPosition(300, 200);
     int window = glutCreateWindow(argv[0]);
@@ -405,11 +202,11 @@ int main(int argc, char *argv[])
         glDebugMessageCallbackARB(DebugFunc, (void*)15);
     }
 
-    init();
+    Init();
 
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(Display);
+    glutReshapeFunc(Reshape);
+    glutKeyboardFunc(Keyboard);
     glutMainLoop();
 
     return 0;
--- shader.vert
+++ shader.vert
@@ -1,7 +1,9 @@
 #version 440
 
-layout (location = 0) in vec3 pos;
+layout (location = 0) in vec2 pos;
 layout (location = 1) in vec3 color;    
+
+uniform mat4 translateMatrixWindowSpace;
 
 // must have the same name as its corresponding "in" item in the frag shader
 smooth out vec3 vertOutColor;
@@ -9,6 +11,7 @@ smooth out vec3 vertOutColor;
 void main()
 {
     vertOutColor = color;
-	gl_Position = vec4(pos, 1.0f);
+    //gl_Position = vec4(pos, -1.0f, 1.0f);
+	gl_Position = translateMatrixWindowSpace * vec4(pos, -1.0f, 1.0f);
 }
 
